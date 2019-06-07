e0e6918308a405f3c17ec6fbb67465371763a4b2
Greg Hull <grhulz@gmail.com>
2019-06-07 05:36:53 -0500

    Axios implemented starting work on the todo controller

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,17 +1,52 @@
 import React, {Component} from "react";
 import {Link} from "react-router-dom";
+import HelloWorldService from "../../api/todo/HelloWorldService.js";
+
 
 class WelcomeComponent extends Component {
+    constructor(props){
+        super(props)
+        this.retrieveWelcomMessage = this.retrieveWelcomMessage.bind(this)
+        this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
+        this.handleError = this.handleError.bind(this)
+        this.state = {
+            welcomeMessage:''
+        }
+    }
   render() {
     return (
         <>
           <h1>Welcome!</h1>
-          <div className="container">
-            Welcome {this.props.match.params.name}. You can manager your todos <Link to="/todos">here</Link>.
-          </div>
+            <div className="container">
+                Welcome {this.props.match.params.name}. You can manager your todos <Link to="/todos">here</Link>.
+            </div>
+            <div className="container">
+                Click here to get a customized welcome message
+                <button onClick={this.retrieveWelcomMessage} className='btn btn-success'>Get Welcome Message</button>
+            </div>
+            <div className="container">
+                {this.state.welcomeMessage}
+            </div>
         </>
     )
   }
+
+    retrieveWelcomMessage(){
+        // HelloWorldService.executeHelloWorldService()
+        //     .then(response => this.handleSuccessfulResponse(response))
+        // HelloWorldService.executeHelloWorldBeanService()
+        //     .then(response => this.handleSuccessfulResponse(response))
+        HelloWorldService.executeHelloWorldPathVariableService(this.props.match.params.name)
+            .then(response => this.handleSuccessfulResponse(response))
+            .catch(error => this.handleError(error))
+  }
+  handleSuccessfulResponse(response){
+        this.setState({welcomeMessage:response.data.message})
+  }
+    handleError(error){
+        // console.log(error.data.message)
+        this.setState({welcomeMessage:error.response.data.message})
+    }
 }
 
 export default WelcomeComponent

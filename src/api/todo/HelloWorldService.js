008c9dd9cda9bebdd7e0365d708462be7c1f10a7
Greg Hull <grhulz@gmail.com>
2019-06-13 06:06:26 -0500

    Start of JWT

--- src/api/todo/HelloWorldService.js
+++ src/api/todo/HelloWorldService.js
@@ -8,15 +8,16 @@ class HelloWorldService {
         return axios.get('http://localhost:8080/hello-world-bean')
     }
     executeHelloWorldPathVariableService(name) {
-        let username = 'in28minutes'
-        let password = 'dummy'
-        let basicAuthHeader = 'Basic ' + window.btoa(username + ':' + password)
-        return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`,
-            {
-                headers:{
-                    authorization:basicAuthHeader
-                }
-            }
+        // let username = 'in28minutes'
+        // let password = 'dummy'
+        // let basicAuthHeader = 'Basic ' + window.btoa(username + ':' + password)
+        return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`
+            // ,
+            // {
+            //     headers:{
+            //         authorization:basicAuthHeader
+            //     }
+            // }
 
             )
     }
--- src/components/todo/AuthenticationService.js
+++ src/components/todo/AuthenticationService.js
@@ -1,25 +1,56 @@
-class AuthenticationService{
-  registerSuccessfulLogin(username, password){
-    sessionStorage.setItem('authenticatedUser', username);
-  }
-  logout(){
-    sessionStorage.removeItem('authenticatedUser');
-  }
-  isUserLoggedIn(){
-    let user = sessionStorage.getItem('authenticatedUser');
-    if(user===null){
-      return false;
-    }else{
-      return true;
+import axios from 'axios'
+
+class AuthenticationService {
+
+    executeBasicAuthenticationService(username, password){
+        return axios.get('http://localhost:8080/basicauth',
+            {headers:{authorization:this.createBasicAuthToken(username,password)}}
+        )
     }
-  }
-    getUsername(){
+
+    createBasicAuthToken(username, password){
+        return 'Basic ' + window.btoa(username + ':' + password)
+    }
+
+    registerSuccessfulLogin(username, password) {
+        sessionStorage.setItem('authenticatedUser', username);
+        this.setupAxiosInterceptors(this.createBasicAuthToken(username,password))
+    }
+
+    logout() {
+        sessionStorage.removeItem('authenticatedUser');
+    }
+
+    isUserLoggedIn() {
         let user = sessionStorage.getItem('authenticatedUser');
-        if(user===null){
+        if (user === null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    getUsername() {
+        let user = sessionStorage.getItem('authenticatedUser');
+        if (user === null) {
             return '';
-        }else{
+        } else {
             return user;
         }
+    }
+
+    setupAxiosInterceptors(basicAuthHeader) {
+
+        axios.interceptors.request.use(
+            (config) => {
+                if (this.isUserLoggedIn()) {
+                    config.headers.authorization = basicAuthHeader
+
+                }
+                return config
+            }
+        )
+
     }
 
 }
